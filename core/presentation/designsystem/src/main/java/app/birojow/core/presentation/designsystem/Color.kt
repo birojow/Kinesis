a11d93a191dd929e15fa836c42f155253908861a
Fabiano Augusto de Mello <f.augmello@hotmail.com>
2024-07-09 23:20:02 -0300

    Add material theme

---
+++ core/presentation/designsystem/src/main/java/app/birojow/core/presentation/designsystem/Color.kt
@@ -0,0 +1,15 @@
+package app.birojow.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val KinesisGreen = Color(0xFF00F15E)
+val KinesisGreen30 = Color(0x4D00F15E)
+val KinesisGreen10 = Color(0x1A00F15E)
+val KinesisGreen5 = Color(0x0D00F15E)
+val KinesisBlack = Color(0xFF080707)
+val KinesisGray = Color(0xFF87938C)
+val KinesisGray40 = Color(0x6687938C)
+val KinesisDarkGray = Color(0xFF232624)
+val KinesisWhite = Color(0xFFFAFAFA)
+val KinesisDarkRed = Color(0xFFBB3D3D)
+val KinesisDarkRed5 = Color(0x0DBB3D3D)
